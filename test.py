48c3e0b010d6ddfd9517faedeaa7ae17ec099e4f
Hojin Park <hojin@uwm.edu>
2020-08-05 18:21:23 +0900

    ver 0.3

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import pickle
 from tensorflow import keras
 from matplotlib import style
 
+
 data = pd.read_csv("student-mat.csv", sep=";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
@@ -18,6 +19,7 @@ y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 best = 0
 
+# Training Process
 for _ in range(30):
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
@@ -39,3 +41,13 @@ linear = pickle.load(pickle_in)
 predictions = linear.predict(x_test)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+# Drawing Best Trained / Acc model
+p = 'G1'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
+
+
